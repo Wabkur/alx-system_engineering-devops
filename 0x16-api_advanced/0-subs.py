daa7f37eb603597b2ef74c249e795e35a33b1c64
wabkur <wabilitukur.com>
2024-08-14 18:56:01 +0300

    api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
-"""script that queries subs on a given reddit subreddit"""
+""" script that queries subs on a given reddit subreddit"""
+
+import requests
 
 
 def number_of_subscribers(subreddit):
     """Return the number of subs on a given subreddit"""
-    import requests
+    base_url = 'https://www.reddit.com'
+    api_uri = '{base}/r/{subreddit}/about.json'.format(base=base_url,
+                                                       subreddit=subreddit)
 
-    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if sub_info.status_code >= 300:
+    user_agent = {'User-Agent': 'Python/requests'}
+
+    res = requests.get(api_uri, headers=user_agent,
+                       allow_redirects=False)
+
+    if res.status_code in [302, 404]:
         return 0
 
-    return sub_info.json().get("data").get("subscribers")
+    return res.json().get('data').get('subscribers')
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,17 +1,30 @@
 #!/usr/bin/python3
 """Module that contain the function top_ten"""
 
+import requests
+
 
 def top_ten(subreddit):
     """Return the top ten posts for a given subreddit"""
-    import requests
+    base_url = 'https://www.reddit.com'
+    api_uri = '{base}/r/{subreddit}/hot.json'.format(base=base_url,
+                                                     subreddit=subreddit)
 
-    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if sub_info.status_code >= 300:
+    user_agent = {'User-Agent': 'Python/requests'}
+
+    payload = {'limit': '10'}
+
+    res = requests.get(api_uri, headers=user_agent,
+                       params=payload, allow_redirects=False)
+
+    if res.status_code in [302, 404]:
         print('None')
     else:
-        [print(child.get("data").get("title"))
-         for child in sub_info.json().get("data").get("children")]
+        res_json = res.json()
+
+        if res_json.get('data') and res_json.get('data').get('children'):
+            hot_posts = res_json.get('data').get('children')
+
+            for post in hot_posts:
+                if post.get('data') and post.get('data').get('title'):
+                    print(post.get('data').get('title'))
