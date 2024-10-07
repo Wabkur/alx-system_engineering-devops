384c155748a4acf15bfa45961c410c219857fc49
wabkur <wabilitukur.com>
2024-10-07 20:12:45 +0300

    advanced

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,30 +1,22 @@
 #!/usr/bin/python3
-"""Module that contain the function top_ten"""
-
+"""Contains top_ten function"""
 import requests
 
 
 def top_ten(subreddit):
-    """Return the top ten posts for a given subreddit"""
-    base_url = 'https://www.reddit.com'
-    api_uri = '{base}/r/{subreddit}/hot.json'.format(base=base_url,
-                                                     subreddit=subreddit)
-
-    user_agent = {'User-Agent': 'Python/requests'}
-
-    payload = {'limit': '10'}
-
-    res = requests.get(api_uri, headers=user_agent,
-                       params=payload, allow_redirects=False)
-
-    if res.status_code in [302, 404]:
-        print('None')
-    else:
-        res_json = res.json()
-
-        if res_json.get('data') and res_json.get('data').get('children'):
-            hot_posts = res_json.get('data').get('children')
-
-            for post in hot_posts:
-                if post.get('data') and post.get('data').get('title'):
-                    print(post.get('data').get('title'))
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
