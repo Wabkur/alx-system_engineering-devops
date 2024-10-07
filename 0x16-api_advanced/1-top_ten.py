c6e62d1561008f9415a9fb63450ac305d2181c8e
wabkur <wabilitukur.com>
2024-10-07 23:44:06 +0300

    advanced

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,32 +1,21 @@
 #!/usr/bin/python3
 """
-Module that contain the function top_ten
+Uses reddit API to get 10 hot posts
 """
 
 import requests
 
 
 def top_ten(subreddit):
-    """Return the top ten posts for a given subreddit"""
-    base_url = 'https://www.reddit.com'
-    api_uri = '{base}/r/{subreddit}/hot.json'.format(base=base_url,
-                                                     subreddit=subreddit)
+    """Get 10 hot posts"""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {"user-agent": "request"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
-    user_agent = {'User-Agent': 'Python/requests'}
+    if response.status_code != 200:
+        print(None)
+        return
 
-    payload = {'limit': '10'}
-
-    response = requests.get(api_uri, headers=user_agent,
-                       params=payload, allow_redirects=False)
-
-    if response.status_code in [302, 404]:
-        print('None')
-    else:
-        res_json = response.json()
-
-        if res_json.get('data') and res_json.get('data').get('children'):
-            hot_posts = res_json.get('data').get('children')
-
-            for post in hot_posts:
-                if post.get('data') and post.get('data').get('title'):
-                    print(post.get('data').get('title'))
+    data = response.json().get("data").get("children")
+    top_10_posts = "\n".join(post.get("data").get("title") for post in data)
+    print(top_10_posts)
